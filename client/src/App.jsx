4fedfab3a321a878f874c82126dbfccdd5827320
achla dhiman <achlar8@gmail.com>
2025-03-24 12:38:30 +1300

    theme setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { Toaster } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 // import { getUserProfileThunk } from "./redux/thunks/user.thunk";
-
+import { useTheme } from './context/ThemeContext';
+import { Menu, X, Sun, Moon } from 'lucide-react';
 function App() {
   
   // const dispatch = useDispatch();
@@ -13,8 +14,18 @@ function App() {
   //   })();
   // }, []);
 
+  const { isDark, toggleTheme } = useTheme();
   return (
     <>
+      <div className="h-screen w-screen overflow-hidden bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      {/* Theme Toggle Button */}
+      <button 
+        onClick={toggleTheme}
+        className="fixed top-4 right-4 z-50 p-2 bg-gray-200 dark:bg-gray-800 rounded-full hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors"
+      >
+        {isDark ? <Sun size={24} /> : <Moon size={24} />}
+      </button>
+      </div>
       <Toaster position="top-center" reverseOrder={false} />
     </>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App.jsx";
 import Router from "./router.jsx";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Provider store={store}>
       <Router />
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </>
 );
